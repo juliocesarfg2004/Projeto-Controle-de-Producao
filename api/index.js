157e262f1354fe1d31ed81b2e17b17aa4a24d493
juliocesarfg <juliocesarfg2004@gmail.com>
2025-11-05 13:53:16 -0300

    commit salvador ?

--- api/index.js
+++ api/index.js
@@ -2,10 +2,10 @@ import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
 
-import usersRouter from "../routes/usuarios.js";
-import produtosRouter from "../routes/produtos.js";
-import produtosTiposRouter from "../routes/produtosTipos.js";
-import ordemProducaoRouter from "../routes/ordemProducao.js";
+import usersRouter from "./routes/usuarios.js";
+import produtosRouter from "./routes/produtos.js";
+import produtosTiposRouter from "./routes/produtosTipos.js";
+import ordemProducaoRouter from "./routes/ordemProducao.js";
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "https://controle-de-producao-front-a6hc04q4q.vercel.app",
-      "http://localhost:5173",
+      "http://localhost:5173"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -31,6 +31,8 @@ app.use((req, res) => {
   res.status(404).json({ error: "Rota não encontrada" });
 });
 
+
 const handler = serverless(app);
+
 export default handler;
 export { handler };
