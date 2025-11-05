d7e5665757ea1eb9ab85e7d88ecca3e92e80e560
juliocesarfg <juliocesarfg2004@gmail.com>
2025-11-04 21:00:52 -0300

    teste

--- api/index.js
+++ api/index.js
@@ -7,9 +7,20 @@ import produtosTiposRouter from "../routes/produtosTipos.js";
 import ordemProducaoRouter from "../routes/ordemProducao.js";
 
 const app = express();
-
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "https://controle-de-producao-front-a6hc04q4q.vercel.app",
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
 
 app.use("/", usersRouter);
 app.use("/", produtosRouter);
