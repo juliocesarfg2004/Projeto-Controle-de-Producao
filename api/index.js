5a5a8e4e98e0888d03bc005c9335044c01960d2c
juliocesarfg <juliocesarfg2004@gmail.com>
2025-11-05 13:30:05 -0300

    commit vercel express

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import serverless from "serverless-http";
 
 import usersRouter from "../routes/usuarios.js";
 import produtosRouter from "../routes/produtos.js";
@@ -7,24 +8,29 @@ import produtosTiposRouter from "../routes/produtosTipos.js";
 import ordemProducaoRouter from "../routes/ordemProducao.js";
 
 const app = express();
+
 app.use(express.json());
 
 app.use(
   cors({
     origin: [
       "https://controle-de-producao-front-a6hc04q4q.vercel.app",
-      "http://localhost:5173"
+      "http://localhost:5173",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.options("*", cors());
 
 app.use("/", usersRouter);
 app.use("/", produtosRouter);
 app.use("/", produtosTiposRouter);
 app.use("/", ordemProducaoRouter);
 
-export default app;
+app.use((req, res) => {
+  res.status(404).json({ error: "Rota não encontrada" });
+});
+
+const handler = serverless(app);
+export default handler;
+export { handler };
