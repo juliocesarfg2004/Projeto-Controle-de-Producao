c6631c3b83b0f159e594be008b5c4b5a32f80ea3
juliocesarfg2004 <juliocesarfg2004@gmail.com>
2026-02-26 15:50:54 -0300

    Adição de middleware

--- API-Fametro/api/index.js
+++ API-Fametro/api/index.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(cors({
   origin: [
     "https://projeto-controle-de-p-git-63c06c-julio-cesars-projects-77a32ca8.vercel.app",
