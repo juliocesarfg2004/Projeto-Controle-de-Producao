af018994475326979874b1fa31013c738aa18a23
juliocesarfg2004 <juliocesarfg2004@gmail.com>
2026-02-26 16:55:20 -0300

    Cors mudados

--- API-Fametro/api/index.js
+++ API-Fametro/api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import serverless from "serverless-http";
 import usersRouter from "./routes/usuarios.js";
 import produtosRouter from "./routes/produtos.js";
@@ -7,20 +8,14 @@ import ordemProducaoRouter from "./routes/ordemProducao.js";
 
 const app = express();
 
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-app.options("/usuarios", (req, res) => res.sendStatus(200));
-app.options("/produtos", (req, res) => res.sendStatus(200));
-app.options("/tipos-produtos", (req, res) => res.sendStatus(200));
-app.options("/ordem-producao", (req, res) => res.sendStatus(200));
-app.options("/login", (req, res) => res.sendStatus(200));
 
 app.use("/", usersRouter);
 app.use("/", produtosRouter);
