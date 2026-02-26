30a1e4dfe4f33a677cb4705f17c301fa7f05424e
juliocesarfg2004 <juliocesarfg2004@gmail.com>
2026-02-26 17:49:28 -0300

    Corrigindo CORS para Vercel

--- API-Fametro/api/index.js
+++ API-Fametro/api/index.js
@@ -8,12 +8,15 @@ import ordemProducaoRouter from "./routes/ordemProducao.js";
 
 const app = express();
 
-app.use(cors({
-  origin: true,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 
