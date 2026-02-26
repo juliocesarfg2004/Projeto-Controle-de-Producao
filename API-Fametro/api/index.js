15334b8e6c5ba853f072d660c8ae1a490b373105
juliocesarfg2004 <juliocesarfg2004@gmail.com>
2026-02-26 16:47:33 -0300

    deploy do backend

--- API-Fametro/api/index.js
+++ API-Fametro/api/index.js
@@ -7,22 +7,26 @@ import ordemProducaoRouter from "./routes/ordemProducao.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
   next();
 });
-
-app.use(express.json());
 
 app.use("/", usersRouter);
 app.use("/", produtosRouter);
 app.use("/", produtosTiposRouter);
 app.use("/", ordemProducaoRouter);
+
+app.all("/(.*)", (req, res) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  return res.status(404).json({ error: "Not Found" });
+});
 
 app.get("/", (req, res) => {
   res.json({ status: "OK", message: "API está funcionando" });
