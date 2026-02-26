e12ffa3257592557cd8e21c9fd3657f4928a7ba1
juliocesarfg2004 <juliocesarfg2004@gmail.com>
2026-02-26 10:58:00 -0300

    Editando a Url da Vercel

--- Front-Fametro/src/services/api.js
+++ Front-Fametro/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-fametro-ggnuq7a3j-julio-cesars-projects-77a32ca8.vercel.app",
+  baseURL: "https://projeto-controle-de-producao.vercel.app",
 });
 
 export default api;
