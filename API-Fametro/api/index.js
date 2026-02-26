a246945fe6c8af86c2cc6fc21cf06b09c60401b6
juliocesarfg2004 <juliocesarfg2004@gmail.com>
2026-02-26 10:54:24 -0300

    Liberando o cors

--- API-Fametro/api/index.js
+++ API-Fametro/api/index.js
@@ -10,13 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.use("/", usersRouter);
 app.use("/", produtosRouter);
