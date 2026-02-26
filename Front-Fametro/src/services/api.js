a41df5c2ed91449a74c31efb48f4a4eb5a7ef515
juliocesarfg2004 <juliocesarfg2004@gmail.com>
2026-02-26 15:35:15 -0300

    Ajustando URL

--- Front-Fametro/src/services/api.js
+++ Front-Fametro/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://projeto-controle-de-p-git-bd750c-julio-cesars-projects-77a32ca8.vercel.app/",
+  baseURL: "https://projeto-controle-de-p-git-cbab1f-julio-cesars-projects-77a32ca8.vercel.app",
 });
 
 export default api;
